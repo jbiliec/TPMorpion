cdef0e478037b75769d3b2f5708090f5965d8a3a
jbiliec <j.simon5660@gmail.com>
2014-10-08 14:49:07 +0200

    Maj Classe View

--- Morpion-git/src/fr/imie/morpion/view/View.java
+++ Morpion-git/src/fr/imie/morpion/view/View.java
@@ -1,9 +1,13 @@
 package fr.imie.morpion.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class View {
 	
@@ -21,6 +25,21 @@ public class View {
 		JPanel headPanel = new JPanel();
 		JPanel scorePanel = new JPanel();
 		JPanel gridPanel = new JPanel();
+		
+		JTextField txtNameA = new JTextField();
+		JTextField txtNameB = new JTextField();
+		JLabel lblNameA = new JLabel("Joueur A:");
+		JLabel lblNameB = new JLabel("Joueur B:");
+		
+		headPanel.setLayout(new GridLayout(1,4));
+		gridPanel.setLayout(new GridLayout(3,3));
+		mainPanel.setLayout(new BorderLayout());
+		
+		headPanel.add(lblNameA);
+		headPanel.add(txtNameA);
+		headPanel.add(lblNameB);
+		headPanel.add(txtNameB);
+		
 	}
 
 }
